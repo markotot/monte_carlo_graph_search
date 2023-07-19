3880d4a081cd03ca851693b653c48924f29bb442
markotot <m.tot@qmul.ac.uk>
2023-07-19 10:35:24 +0100

    ci: renamed apptainer.def and apptainer.sif files

--- scripts/apocrita_build_and_run.sh
+++ scripts/apocrita_build_and_run.sh
@@ -20,7 +20,7 @@ spawn ssh -i $APOC_PRIVATE_KEY $APOC_USERNAME@login.hpc.qmul.ac.uk \
  source ../../../../../etc/bashrc; \
  rm myenvs; \
  echo NEPTUNE_API_TOKEN=$NEPTUNE_API_TOKEN > myenvs; \
- apptainer build --force gaig-pipeline.sif $PROJECT_NAME/apptainer/gaig-pipeline.def; \
+ apptainer build --force apptainer.sif $PROJECT_NAME/apptainer/apptainer.def; \
  qsub $PROJECT_NAME/scripts/submit_job.sh;
  "
 expect "Enter passphrase for key '$APOC_PRIVATE_KEY':"
