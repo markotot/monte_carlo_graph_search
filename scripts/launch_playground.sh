dbc2d259071804c22c797b3de5ab387683948f9e
markotot <m.tot@qmul.ac.uk>
2024-03-01 16:54:42 +0000

    fix: testing passing of seed

--- scripts/launch_playground.sh
+++ scripts/launch_playground.sh
@@ -1,6 +1,4 @@
 #params="training.training_steps=10,100,1000,10000"
 #python3 monte_carlo_graph_search/experiments/main.py --multirun $params
-echo "LAUNCH playground"
-echo $SEED
-echo "LAUNCH playground"
+
 python3 -m experiments.playground $SEED
--- scripts/submit_array_job.sh
+++ scripts/submit_array_job.sh
@@ -9,7 +9,5 @@
 
 module load python/3.8.5
 
-
 # Replace the following line with a program or command
-echo "SGE_TASK_ID: $SGE_TASK_ID"
-apptainer run --env-file myenvs --env "SEED=$SGE_TASK_ID" playground.sif
+apptainer run --env-file myenvs --env "SEED=$SGE_TASK_ID" mcgs.sif
