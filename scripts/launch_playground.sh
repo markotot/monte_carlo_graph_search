f9fc091ade22184b49193017ec86ced352293e25
markotot <m.tot@qmul.ac.uk>
2024-03-01 16:26:01 +0000

    fix: testing passing of seed

--- scripts/launch_playground.sh
+++ scripts/launch_playground.sh
@@ -1,6 +1,6 @@
 #params="training.training_steps=10,100,1000,10000"
 #python3 monte_carlo_graph_search/experiments/main.py --multirun $params
-echo "Running playground"
+echo "LAUNCH playground"
 echo $SEED
-echo "SEED"
+echo "LAUNCH playground"
 python3 -m experiments.playground $SEED
