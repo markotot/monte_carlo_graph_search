3bfa33f75db17a623c29cf8a233afc6abe076569
markotot <m.tot@qmul.ac.uk>
2024-03-03 00:50:22 +0000

    fix: fixing the issue where run id doesn't go through to the main.py

--- scripts/apocrita_build_and_run_and_aggregate.sh
+++ scripts/apocrita_build_and_run_and_aggregate.sh
@@ -26,9 +26,9 @@ spawn ssh -i $APOC_PRIVATE_KEY $APOC_USERNAME@login.hpc.qmul.ac.uk \
  rm myenvs; \
  echo NEPTUNE_API_TOKEN=$NEPTUNE_API_TOKEN > myenvs; \
  apptainer build --force mcgs.sif $PROJECT_NAME/apptainer/mcgs.def; \
- qsub -t $START_SEED-$END_SEED -N $RUN_NAME $PROJECT_NAME/scripts/submit_array_job.sh; $RUN_NAME\
+ qsub -t $START_SEED-$END_SEED -N $RUN_NAME -v RUN_NAME=$RUN_NAME $PROJECT_NAME/scripts/submit_array_job.sh;\
  apptainer build --force aggregate_data.sif $PROJECT_NAME/apptainer/aggregate_data.def; \
- qsub -hold_jid ${RUN_NAME} -N ${RUN_NAME}-Aggregate $PROJECT_NAME/scripts/submit_aggregate_job.sh $RUN_NAME $START_SEED $END_SEED; \
+ qsub -hold_jid ${RUN_NAME} -N ${RUN_NAME}-Aggregate -v RUN_NAME=$RUN_NAME,START_ID=$START_SEED,END_ID=$END_SEED $PROJECT_NAME/scripts/submit_aggregate_job.sh; \
  "
 expect "Enter passphrase for key '$APOC_PRIVATE_KEY':"
 send "$APOC_PASSPHRASE\r"
