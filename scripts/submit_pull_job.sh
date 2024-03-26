ab379a74da52f978c872fedb1d8e4ac5de22261b
markotot <m.tot@qmul.ac.uk>
2024-03-26 10:41:07 +0000

    exp: testing new scripts with separater pull, experiment, aggregate

--- scripts/submit_pull_job.sh
+++ scripts/submit_pull_job.sh
@@ -9,4 +9,4 @@
 module load python/3.8.5
 
 # Replace the following line with a program or command
-apptainer run --env-file myenvs --env "JOB_TYPE=$JOB_TYPE,RUN_NAME=$RUN_NAME,START_ID=$START_ID,END_ID=$END_ID,SEED=$SEED" containers/pure_mcgs.sif
+apptainer run --env-file myenvs --env "JOB_TYPE=$JOB_TYPE,RUN_NAME=$RUN_NAME,START_ID=$START_ID,END_ID=$END_ID,SEED=$SGE_TASK_ID" containers/pure_mcgs.sif
