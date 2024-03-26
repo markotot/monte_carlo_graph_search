684ad4d683327752bb7a4d0fe2e71d511c50187d
markotot <m.tot@qmul.ac.uk>
2024-03-26 12:02:37 +0000

    feat: moving apptainer creation to qsub

--- scripts/apocrita_build.sh
+++ scripts/apocrita_build.sh
@@ -9,6 +9,8 @@ set GIT_BRANCH [lindex $argv 4];
 set PROJECT_NAME [lindex $argv 5];
 set NEPTUNE_API_TOKEN [lindex $argv 6];
 
+BUILD_JOB_PARAMS="-N ${APPTAINER}-Build -v PROJECT_NAME=$PROJECT_NAME,GIT_BRANCH=$GIT_BRANCH,JOB_TYPE=\"build\" $PROJECT_NAME/scripts/submit_build_job.sh"
+
 spawn ssh -i $APOC_PRIVATE_KEY $APOC_USERNAME@login.hpc.qmul.ac.uk \
  "
  cd $PROJECT_NAME; \
@@ -20,7 +22,7 @@ spawn ssh -i $APOC_PRIVATE_KEY $APOC_USERNAME@login.hpc.qmul.ac.uk \
  source ../../../../../etc/bashrc; \
  rm myenvs; \
  echo NEPTUNE_API_TOKEN=$NEPTUNE_API_TOKEN > myenvs; \
- apptainer build --force containers/pure_mcgs.sif $PROJECT_NAME/apptainer/mcgs.def; \
+ qsub $BUILD_JOB_PARAMS; \
  "
 expect "Enter passphrase for key '$APOC_PRIVATE_KEY':"
 send "$APOC_PASSPHRASE\r"
---
+++ scripts/submit_build_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -pe smp 8
+#$ -l h_vmem=1G
+#$ -l h_rt=1:0:0
+#$ -cwd
+#$ -j y
+#$ -o job_results
+
+module load python/3.8.5
+
+# Replace the following line with a program or command
+APPTAINERENV_NSLOTS=${NSLOTS}
+apptainer build --force containers/pure_mcgs.sif $PROJECT_NAME/apptainer/mcgs.def;
