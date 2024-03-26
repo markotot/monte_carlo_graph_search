49491957c5faa08d3d055d2a60f090755cd8d716
markotot <m.tot@qmul.ac.uk>
2024-03-26 10:18:05 +0000

    feat: changing names of bash script variables

--- scripts/launch_exp.sh
+++ scripts/launch_exp.sh
@@ -1,4 +1,4 @@
 #params="training.training_steps=10,100,1000,10000"
 #python3 monte_carlo_graph_search/experiments/main.py --multirun $params
 
-python3 -m experiments.main run_name=$1 env.seed=$2 search.seed=$2
+python3 -m experiments.main run_name=$RUN_NAME env.seed=$SEED search.seed=$SEED
