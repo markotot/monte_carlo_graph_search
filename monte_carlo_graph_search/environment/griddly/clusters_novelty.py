97506af880bf306f1e5d6571cfa1bc3dc664b7b8
markotot <m.tot@qmul.ac.uk>
2024-04-02 10:39:26 +0100

    feat: added gclusters novelty

---
+++ monte_carlo_graph_search/environment/griddly/clusters_novelty.py
@@ -0,0 +1,27 @@
+class ClustersNovelty:
+    def __init__(self, config):
+        self.total_data_points = 0
+        self.threshold = config.novelty_threshold
+
+    def calculate_novelty(self, observation):
+        return 0
+
+    def check_if_novel(self, feature_value, feature_name):
+        return 0
+
+    def pseudo_count_novelty_function(self, observation):
+        return 0
+
+    def update_posterior(self, observation, done, nodes, move, forward_model_calls):
+
+        return 0
+        self.total_data_points += 1
+
+    def get_discovered_subgoals(self):
+        return {}
+
+    def discover_subgoal(self, subgoal_name, nodes, move, forward_model_calls):
+        pass
+
+    def get_subgoal_metrics(self):
+        return {}
